c1cfda7a00d2b36c283c4b19a926a2fbd13b0cc4
Chanu Perera <78588327+ChanuPerera@users.noreply.github.com>
2023-07-19 10:44:53 +0530

    Quiz Commit ID #2

--- src/Routes/Quize.jsx
+++ src/Routes/Quize.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 function Quize() {
+  const [activeQuiz, setActiveQuiz] = useState(0);
+
+  const handleQuizItemClick = (index) => {
+    setActiveQuiz(index);
+  };
+
+  const quizItems = [
+    { number: "01", lecture: "Introduction to Programming" },
+    { number: "02", lecture: "Data Types and C Formatted Input/Output" },
+    { number: "03", lecture: "Operations in C" },
+    { number: "04", lecture: "Selection Statements in C" },
+    { number: "05", lecture: "Repetition Statements in C" },
+    { number: "06", lecture: "Functions" },
+    { number: "07", lecture: "Array" },
+    { number: "08", lecture: "File Handling" },
+  ];
+
   return (
     <div className="main-wrapper w-full h-full bg-slate-100 mt-[96px] ">
       <div className="main-body-wrapper text-center items-center">
         <div className="body-content text-center h-full">
           <div className="quize-form-body w-full flex flex-col bg-slate-100 h-full space-y-5">
-
             <div className="process-nav flex flex-row h-[110px] bg-white w-full shadow-md shadow-slate-300 ">
-
               <div className="process p-3 flex flex-row justify-center items-center w-1/3">
                 <div className="w-[70%] mx-auto flex flex-row justify-center items-center">
                   <div className="w-full flex flex-col text-left ">
@@ -21,8 +36,10 @@ function Quize() {
                       Lorem ipsum dolor sit amet.
                     </h5>
                   </div>
-                  <div className="w-full">
-                   <span className="text-[18pt] text-green-600"><FontAwesomeIcon icon={faCheck} /></span> 
+                  <div className="w-1/4">
+                    <span className="text-[18pt] text-green-600">
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
                   </div>
                 </div>
               </div>
@@ -37,8 +54,10 @@ function Quize() {
                       Lorem ipsum dolor sit amet.
                     </h5>
                   </div>
-                  <div className="w-full">
-                   <span className="text-[18pt] text-green-600"><FontAwesomeIcon icon={faCheck} /></span> 
+                  <div className="w-1/4">
+                    <span className="text-[18pt] text-green-600">
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
                   </div>
                 </div>
               </div>
@@ -53,114 +72,42 @@ function Quize() {
                       Lorem ipsum dolor sit amet.
                     </h5>
                   </div>
-                  <div className="w-full">
-                   <span className="text-[18pt] text-green-600"><FontAwesomeIcon icon={faCheck} /></span> 
+                  <div className="w-1/4">
+                    <span className="text-[18pt] text-green-600">
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
                   </div>
                 </div>
               </div>
-
             </div>
 
             <div className="quize-panel p-5 w-full h-full flex flex-row bg-white ">
-              <div className="quize-items p-2 h-full w-1/4 space-y-5  border-r-[1px] border-slate-300 bg-white ">
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-blue-950 rounded-full justify-center items-center flex shadow-md shadow-slate-400">
-                    <h4 className="font-semibold text-white ">01</h4>
+              <div className="quize-items p-2 h-full w-1/4 space-y-5   ">
+                {quizItems.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2 cursor-pointer ${
+                      activeQuiz === index ? "active" : "inactive"
+                    }`}
+                    onClick={() => handleQuizItemClick(index)}
+                  >
+                    <div className="quize-item-number w-[48px] h-[48px] rounded-full justify-center items-center flex shadow-md shadow-slate-400">
+                      <h4 className="font-semibold">
+                        {item.number}
+                      </h4>
+                    </div>
+                    <div className="q-name w-full flex flex-col space-y-1 text-left">
+                      <span>Lecture {item.number}</span>
+                      <h3 className="font-semibold">{item.lecture}</h3>
+                      {/* <h3>{item.title}</h3> */}
+                    </div>
                   </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left ">
-                    <span className="text-[#1f1f71]">Lecture 01</span>
-                    <h3 className="text-[#1f1f71] font-semibold text-[14pt]">
-                      Introduction to Programming
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-white border-2 rounded-full justify-center items-center flex">
-                    <h4 className="font-semibold text-[#828282] ">02</h4>
-                  </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left">
-                    <span className="text-[#828282]">Lecture 02</span>
-                    <h3 className="text-[#828282] font-semibold text-[14pt]">
-                      Data Types and C Formatted Input/Output
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-white border-2 rounded-full justify-center items-center flex">
-                    <h4 className="font-semibold text-[#828282] ">03</h4>
-                  </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left">
-                    <span className="text-[#828282]">Lecture 03</span>
-                    <h3 className="text-[#828282] font-semibold text-[14pt]">
-                      Operations in C
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-white border-2 rounded-full justify-center items-center flex">
-                    <h4 className="font-semibold text-[#828282] ">04</h4>
-                  </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left">
-                    <span className="text-[#828282]">Lecture 04</span>
-                    <h3 className="text-[#828282] font-semibold text-[14pt]">
-                      Selection Statements in C
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-white border-2 rounded-full justify-center items-center flex">
-                    <h4 className="font-semibold text-[#828282] ">05</h4>
-                  </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left">
-                    <span className="text-[#828282]">Lecture 05</span>
-                    <h3 className="text-[#828282] font-semibold text-[14pt]">
-                      Repetation Statements in C
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-white border-2 rounded-full justify-center items-center flex">
-                    <h4 className="font-semibold text-[#828282] ">06</h4>
-                  </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left">
-                    <span className="text-[#828282]">Lecture 06</span>
-                    <h3 className="text-[#828282] font-semibold text-[14pt]">
-                      Functions
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-white border-2 rounded-full justify-center items-center flex">
-                    <h4 className="font-semibold text-[#828282] ">07</h4>
-                  </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left">
-                    <span className="text-[#828282]">Lecture 07</span>
-                    <h3 className="text-[#828282] font-semibold text-[14pt]">
-                      Array
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="quize-item-card flex flex-row w-full justify-center items-center space-x-5 p-2">
-                  <div className="quize-item-number w-[48px] h-[48px] bg-white border-2 rounded-full justify-center items-center flex">
-                    <h4 className="font-semibold text-[#828282] ">08</h4>
-                  </div>
-                  <div className="q-name w-full flex flex-col space-y-1 text-left">
-                    <span className="text-[#828282]">Lecture 08</span>
-                    <h3 className="text-[#828282] font-semibold text-[14pt]">
-                      File Handling
-                    </h3>
-                  </div>
-                </div>
+                ))}
               </div>
 
-              <div className="quize-area p-5 w-3/4  flex flex-col ">
+
+
+              <div className="quize-area p-5 w-3/4  flex flex-col border-l-[1px] border-slate-300 bg-white">
                 <div className="qa-head p-5 flex flex-col text-left border-b-[1px] border-gray-300">
                   <h3 className="text-[#1f1f71] text-[14pt]">Lecture 01</h3>
                   <h2 className="text-[#1f1f71] font-semibold text-[20pt]">
